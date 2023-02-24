905e05a4b143696eca2891972d80e64cfd3f32ed
Devesh Kumar Mishra <94969888+devn5900@users.noreply.github.com>
2023-02-24 16:23:57 +0530

    footer-done,search-fix

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ import Navbar from "./components/Navbar";
 import AllRoutes from "./Routes/AllRoutes";
 import Footer from "./components/Footer";
 import AdminNavbar from "./components/Admin_components/AdminNavbar";
-<<<<<<< HEAD
 import { useLocation } from "react-router-dom";
-=======
-import Register from "./pages/Authentication/Register"
->>>>>>> 591786f95332ff63b26c113df7cf646048190586
+import Register from "./pages/Authentication/Register";
 function App() {
   const location = useLocation();
   const toggleNav = location.pathname.includes("admin");
@@ -19,12 +16,9 @@ function App() {
       {!toggleNav ? <Navbar /> : <AdminNavbar />}
       {/* add your path in Routes/AllRoutes.jsx */}
       <AllRoutes />
-<<<<<<< HEAD
       <Footer />
-=======
       {/* <Register/> */}
       {/* <Footer /> */}
->>>>>>> 591786f95332ff63b26c113df7cf646048190586
     </Box>
   );
 }
