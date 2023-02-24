55e6d7f9c81adc57f91fba6fe32e797389f83fb3
Manoj Kumar <officailmanoj14@gmail.com>
2023-02-25 04:44:38 +0530

    added mens single page

---
+++ src/pages/MensSinglePage/MensSinglePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MensSinglePage = () => {
+  return (
+    <div>
+      <h1>This is mens single page.</h1>
+    </div>
+  )
+}
+
+export default MensSinglePage;
---
+++ src/pages/womens/womens.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const womens = () => {
+  return (
+    <div>
+      <h1>This is womens page</h1>
+    </div>
+  )
+}
+
+export default womens;
