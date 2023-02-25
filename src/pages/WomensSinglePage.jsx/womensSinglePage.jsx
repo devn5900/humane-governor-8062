7cd470abfda6104aecda12f85dbdd6b220234a35
Manoj Kumar <officailmanoj14@gmail.com>
2023-02-26 03:04:37 +0530

    added single womens page

---
+++ src/pages/WomensSinglePage.jsx/womensSinglePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const womensSinglePage = () => {
+  return (
+    <div>
+      <h1>This is a single womens page.</h1>
+    </div>
+  )
+}
+
+export default womensSinglePage;
