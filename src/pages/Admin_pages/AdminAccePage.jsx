8d4bfb42c73d24766bdeedef3551e8378f95a4b6
deepti1144 <dipz09085@gmail.com>
2023-02-26 22:18:31 +0530

    Admin-final

--- src/pages/Admin_pages/AdminAccePage.jsx
+++ src/pages/Admin_pages/AdminAccePage.jsx
@@ -56,7 +56,10 @@ const AdminAccePage = () => {
     },1000)
     })
   }
-  const handleOpen=(id)=>{
+  const handleOpen=(id,name , discount , price)=>{
+    setTitle(name)
+    setDiscount(discount)
+    setPrice(price)
     setId(id);
     onOpen();
 
